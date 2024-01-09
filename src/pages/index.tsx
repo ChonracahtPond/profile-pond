18160c174786837dbccf07b08518f9308bf37371
ChonracahtPond <137247404+ChonracahtPond@users.noreply.github.com>
2024-01-09 15:09:39 +0700

    pond

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,27 +16,45 @@ export default function Home() {
   return (
     <RootLayout>
       <>
-        <div className="flex min-h-screen items-center justify-center bg-gradient-to-tr to-blue-700 from-indigo-900 p-10">
-          <div className=" w-max">
+        {/* <div className="flex min-h-screen items-center justify-center bg-gradient-to-tr to-blue-700 from-indigo-900 p-10 ">
+          <div className=" w-max ">
             <h1
               className="animate-typing overflow-hidden whitespace-nowrap border-r-4 border-r-white pr-5 text-5xl text-white font-bold">
-                 My  Name  Is
-              {/* Welcome To Profile */}
+              My  Name  Is
+
             </h1>
             <h1
               className="animate-typing overflow-hidden whitespace-nowrap border-r-4 border-r-white pr-5 text-5xl text-white font-bold mt-5">
-                Mr.Chonracaht Intarasut
-              {/* Welcome To Profile */}
+              Mr.Chonracaht Intarasut
+
+            </h1>
+          </div>
+        </div> */}
+
+        {/* Welcome To Profile */}
+        {/* Welcome To Profile */}
+
+        <div className="flex min-h-screen items-center justify-center bg-gradient-to-tr to-blue-700 from-indigo-900 p-10 sm:text-center ">
+          <div className="w-max ">
+            <h1 className="animate-typing overflow-hidden whitespace-nowrap border-r-4 border-r-white pr-5 text-3xl sm:text-5xl text-white font-bold">
+              My Name Is
+       
+            </h1>
+            <h1 className="animate-typing overflow-hidden whitespace-nowrap border-r-4 border-r-white pr-5 text-3xl sm:text-5xl text-white font-bold mt-5">
+              Mr. Chonracaht Intarasut
+       
+
             </h1>
           </div>
         </div>
 
-        <MenuIcon/>
-        <ShowCard/>
-        <MenuPages/>
-        <ProgressBar/>
+
+        <MenuIcon />
+        <ShowCard />
+        <MenuPages />
+        <ProgressBar />
         <Stepper />
-       
+
         {/* <Slider /> */}
         <Sliderpages />
 
